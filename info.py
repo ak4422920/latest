665d4397853991babd0e8cc2f01dabdc69e82434
ak4422920 <ak4422920@gmail.com>
2025-05-06 17:08:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,8 +66,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'akmovies')
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://akm:akm@cluster0.wf6fy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://ak:ak@cluster0.9zbdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For File Data Store
+O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://ak:ak@cluster0.oghxxl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For Other Data Store
+F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://akm:akm@cluster0.wf6fy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For File Data Store
 S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://db:db@cluster0.oghxxl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
